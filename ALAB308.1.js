6a0dd3051fb31be1c7991bf9c25fdb4a59b52d73
Xinyi Ouyang <oyxyddd@gamil.com>
2024-01-03 11:16:44 -0500

    Finished Part 3

--- ALAB308.1.js
+++ ALAB308.1.js
@@ -19,22 +19,24 @@ try{
 const bodyEl = document.querySelector('body');
 const txtEl = bodyEl.appendChild(document.createElement('h2'));
 function primeList(n){
-    if (n == 1) {
-        return [1]
+    if (n == 1) {    
+        return txtEl.textContent += '1';
     } else if (n == 2) {
-        return [2,1]
+        return txtEl.textContent += '2,1';
     }
 
     for (let j = 2; j < n; j++ ){
         if (n % j === 0 ){
-        return primeList(n-1)
-        break;
+        //   console.log(j,n);
+        //   console.log(txtEl.textContent)
+         return primeList(n-1),100;
         }
-        return [n,primeList(n-1)];
+        txtEl.textContent = txtEl.textContent + n +',' 
+        return primeList(n-1),100;
     }
     }
 
 // console.log(primeList(10).flat(Infinity).toString())
-txtEl.textContent = primeList(5000).flat(Infinity).toString()
-console.log(txtEl.textContent)
-alert('Calculation is finished !') 
+// txtEl.textContent = primeList(5000).flat(Infinity).toString()
+primeList(1000)
+setTimeout(alert,0,'Calculation is finished !')
